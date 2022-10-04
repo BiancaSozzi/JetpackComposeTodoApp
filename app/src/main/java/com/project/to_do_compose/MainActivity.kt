ed442c9d5a5491f71f752e868fbea47c5bcaba3f
BiancaSozzi <biancasozzi@gmail.com>
2022-10-04 10:59:50 +0200

    configure hilt

--- app/src/main/java/com/project/to_do_compose/MainActivity.kt
+++ app/src/main/java/com/project/to_do_compose/MainActivity.kt
@@ -3,15 +3,10 @@ package com.project.to_do_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.project.to_do_compose.ui.theme.TodocomposeTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

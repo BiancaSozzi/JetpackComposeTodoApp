aba1cf9a3693d5e60f51116ae7301173ff89f9b6
BiancaSozzi <biancasozzi@gmail.com>
2022-10-06 13:59:06 +0200

    Create search bar

--- app/src/main/java/com/project/to_do_compose/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/project/to_do_compose/ui/screens/list/ListAppBar.kt
@@ -1,14 +1,25 @@
 package com.project.to_do_compose.ui.screens.list
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.AppBarDefaults
+import androidx.compose.material.ContentAlpha
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldColors
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -23,8 +34,13 @@ import com.project.to_do_compose.ui.theme.topAppBarContentColor
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import com.project.to_do_compose.components.PriorityItem
 import com.project.to_do_compose.ui.theme.LARGE_PADDING
+import com.project.to_do_compose.ui.theme.TOP_APP_BAR_HEIGHT
 import com.project.to_do_compose.ui.theme.Typography
 
 @Composable
@@ -165,9 +181,87 @@ fun DeleteAllAction(
     }
 }
 
+@Composable
+fun SearchAppBar(
+    text: String,
+    onTextChanged: (String) -> Unit,
+    onCloseClicked: () -> Unit,
+    onSearchClicked: (String) -> Unit
+) {
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(TOP_APP_BAR_HEIGHT),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.topAppBarBackgroundColor
+    ) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth(),
+            value = text,
+            onValueChange = { onTextChanged(it) },
+            placeholder = {
+                Text(
+                    text = stringResource(id = R.string.search_placeholder_text),
+                    color = Color.White,
+                    modifier = Modifier.alpha(ContentAlpha.medium)
+                )
+            },
+            textStyle = TextStyle(
+                color = MaterialTheme.colors.topAppBarContentColor,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            ),
+            singleLine = true, // text will not occupy multiple lines
+            leadingIcon = {
+                IconButton(
+                    onClick = { onSearchClicked(text) },
+                    modifier = Modifier.alpha(ContentAlpha.disabled)
+                ) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                        contentDescription = stringResource(id = R.string.search_button),
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = { onCloseClicked() }) {
+                    Icon(
+                        imageVector = Icons.Filled.Close,
+                        contentDescription = stringResource(id = R.string.close_search_button),
+                        tint = MaterialTheme.colors.topAppBarContentColor
+                    )
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                // It will include the search button on the keyboard
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                // When the user clicks on the search button of the keyboard, it should trigge the search
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colors.topAppBarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                backgroundColor = Color.Transparent
+            )
+        )
+    }
+}
 
 @Composable
 @Preview
 private fun DefaultListAppBarPreview() {
     DefaultListAppBar(onSearchClicked = {}, onSortClicked = {}, onDeleteClicked = {})
+}
+
+@Composable
+@Preview
+private fun SearchAppBarPreview() {
+    SearchAppBar(text = "Search", onTextChanged = {}, onCloseClicked = {}, onSearchClicked = {})
 }
